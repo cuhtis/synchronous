67f24344a6108afa62a1df4e9eb501e18405c0e7
Noah <noahcheng@yahoo.com>
2016-05-13 01:07:17 -0400

    streaming music-static

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 * Created by Noah Cheng, Curtis Li
 
 ### Work in Progress
+* currently serves a static mp3 file.
+* go to localhost:3000/stream
 
 ## Features
 
---
+++ synchronous/app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
+var ms = require('mediaserver');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Sass middleware
+var srcPath = __dirname + '/public/scss';
+var destPath = __dirname + '/public/';
+
+app.use(sassMiddleware({
+  src: srcPath,
+  dest: destPath,
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.get('/music.mp3', function(req, res){
+  ms.pipe(req, res, "/music.mp3");
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
