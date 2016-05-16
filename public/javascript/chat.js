5aaa928a548766ce634c63b3643b119683ffc825
Curtis <curtis99877@gmail.com>
2016-05-16 19:04:32 -0400

    move css and js to own files

---
+++ public/javascript/chat.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var socket = io();
+  var chatForm = $('#chat-form');
+  var m = $('#m');
+  var messages = $('#messages');
+  messages.height($(window).height() - chatForm.height() - 7);
+
+  chatForm.submit(function(){
+    if(m.val() == "") return false;
+    socket.emit('chat message', m.val());
+    m.val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    messages.append($('<li>').text(msg));
+    messages.scrollTop(messages.prop("scrollHeight"));
+  });
+});
---
+++ public/javascript/stream.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var socket = io();
+  var audio = document.getElementById("audioplayer");
+  var seeking = false;
+
+  // Listen events
+  socket.on('play', function(msg) {
+    audio.play();
+  });
+  socket.on('pause', function(msg) {
+    audio.pause();
+  });
+  socket.on('seek', function(time) {
+    audio.currentTime = time;
+  });
+
+  // User events
+  audio.addEventListener("play", function(event) {
+    socket.emit("play");
+  });
+  audio.addEventListener("pause", function(event) {
+    socket.emit("pause");
+  });
+  audio.addEventListener("seeking", function(event) {
+    seeking = true;
+  });
+  audio.addEventListener("seeked", function(event) {
+    if (seeking)
+      socket.emit("seek", audio.currentTime);
+    seeking = false;
+  });
+});
